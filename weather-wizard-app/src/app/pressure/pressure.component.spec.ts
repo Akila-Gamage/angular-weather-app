1c9f7f8838abf67117272013637acc960af2bbc6
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-06-18 21:39:35 +0530

    Create pressure.component.spec.ts

---
+++ weather-wizard-app/src/app/pressure/pressure.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PressureComponent } from './pressure.component';
+
+describe('PressureComponent', () => {
+  let component: PressureComponent;
+  let fixture: ComponentFixture<PressureComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PressureComponent]
+    });
+    fixture = TestBed.createComponent(PressureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
