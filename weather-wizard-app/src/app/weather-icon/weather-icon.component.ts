d06504c50ab36b8ea7cf68fef2632020e2ab5ff3
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-06-21 20:33:50 +0530

    Create weather-icon.component.ts

---
+++ weather-wizard-app/src/app/weather-icon/weather-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-icon',
+  templateUrl: './weather-icon.component.html',
+  styleUrls: ['./weather-icon.component.css']
+})
+export class WeatherIconComponent {
+  @Input() iconUrl: any;
+}
